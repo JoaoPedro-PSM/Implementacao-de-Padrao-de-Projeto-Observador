47d1296fab3242e31e729c808500df5a7ffd3dc0
João Pedro Pereira de Souza Mila <109964889+JoaoPedro-PSM@users.noreply.github.com>
2025-11-18 00:43:05 -0400

    adicao das funcoes verificarpreco e atualizapreco
    fiz a adiçao das duas funções  verificarPreco e atualizarPreco que juntas Simulam um sistema de notificação numa relação onde Loja(publisher) notifica os Clientes(observer)  das alterações
    
    o problema é que para fazer isso a função tem que executar e notificar cada cliente manualmente executando em sequencia a funcao verificar preco para cada cliente.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,16 @@ class Cliente {
         this.nome = nome;
     }
 
+    public verificarPreco(fruta: string, preco: number): void {
+        console.log(`O preço da ${fruta} foi atualizado.`);
+        console.log(`Preço atual: R$ ${preco.toFixed(2)}.`);
+
+        if (preco < 5.00) {
+            console.log(`${this.nome}:comprar ${fruta} o preço esta mt bom`);
+        } else {
+            console.log(`esta mt caro, vou esperar`);
+        }
+    }
 }
 
 class LojaDeFrutas {
@@ -24,6 +34,15 @@ class LojaDeFrutas {
     public getPreco(fruta: string): number {
         return this.precos[fruta] || 0;
     }
+
+    public atualizarPreco(fruta: string, novoPreco: number): void {
+        console.log(`atualizando o preço de ${fruta} para ${novoPreco.toFixed(2)}`);
+        this.precos[fruta] = novoPreco;
+
+        this.clientes.forEach(cliente => {
+            cliente.verificarPreco(fruta, novoPreco); 
+        });
+    }
 }
 
 const loja = new LojaDeFrutas();
@@ -35,3 +54,6 @@ const carlos = new Cliente('Carlos');
 loja.adicionarCliente(joao);
 loja.adicionarCliente(maria);
 loja.adicionarCliente(carlos);
+
+loja.atualizarPreco('Manga',4);
+loja.atualizarPreco('Morango',8);
